cf39648109ea6de1ce8ed92da3713751c1510a1f
Petar <echonykrom@protonmail.com>
2022-01-21 13:44:22 +0200

    remove redundant code

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=p%5)yvj(wm)qrb$5gh#j2!0vl6h$!z6jkcb5ddei#&lm=l_(u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
